9445a14200fe5c3a2108dc9ca6e5cad2437ecc00
Yash Bhanushali <yashbhadra1@gmail.com>
2023-10-27 12:12:13 +0530

    4.added active styles with state management

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -2,10 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeColor, changeBrushSize } from "@/slice/toolboxSlice";
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/constants";
+import classNames from "classnames";
 
 const Toolbox = () => {
   const dispatch = useDispatch();
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const { color } = useSelector((state) => state.toolbox[activeMenuItem]);
 
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL.name;
   const showBrushToolOption =
@@ -24,14 +26,20 @@ const Toolbox = () => {
         <div className={styles.toolItem}>
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
-            {Object.keys(COLORS).map((colorKey) => (
-              <div
-                key={COLORS[colorKey]}
-                className={styles.colorBox}
-                style={{ backgroundColor: COLORS[colorKey] }}
-                onClick={() => updateColor(COLORS[colorKey])}
-              />
-            ))}
+            {Object.keys(COLORS).map((colorKey) => {
+              const colorBoxClasses = classNames(styles.colorBox, {
+                [styles.active]: COLORS[colorKey] === color,
+              });
+
+              return (
+                <div
+                  key={COLORS[colorKey]}
+                  className={colorBoxClasses}
+                  style={{ backgroundColor: COLORS[colorKey] }}
+                  onClick={() => updateColor(COLORS[colorKey])}
+                />
+              );
+            })}
           </div>
         </div>
       )}
